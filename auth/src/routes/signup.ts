be803e262c55f33a87b1d26e86ebb20399202391
Olusegun-Light <lightolusegun1@gmail.com>
2024-08-14 20:31:35 +0100

    task: built microservice architecture

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+
 import { RequestValidationError } from "../errors/request-validation";
 import { User } from "../models/users";
 import { BadRequestError } from "../errors/bad-request-error";
@@ -27,13 +29,27 @@ router.post(
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      throw new BadRequestError("Email in use")
+      throw new BadRequestError("Email in use");
     }
 
-    const user = User.build({email, password})
-    await user.save()
+    const user = User.build({ email, password });
+    await user.save();
 
-    res.status(201).send(user)
+    // Generate jwt
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      "whfio"
+    );
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(201).send(user);
   }
 );
 
